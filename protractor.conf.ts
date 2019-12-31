d28c976d07f2cd4a3d5cefb05615c8ba65bb6c59
BhattacharyaCodes <puja.bhattacharya.pb@gmail.com>
2019-12-31 09:30:52 +0530

    conf file to run the spec

---
+++ protractor.conf.ts
@@ -0,0 +1,37 @@
+import { browser, Config, ProtractorBrowser } from 'protractor';
+import protractor = require('protractor');
+
+var HtmlReporter = require('protractor-beautiful-reporter');
+
+export const config: Config = {
+  directConnect: true,
+   capabilities: {
+       'browserName': 'chrome',
+        chromeOptions: {
+             args: [ "--headless", "--disable-gpu" ]
+       },
+   },
+   framework: 'jasmine',
+   specs: [
+       './e2e_test_suite/spec/maps.spec.js'    
+    ],
+
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 120000
+    },
+    onPrepare: async() => {
+        jasmine.getEnv().addReporter(new HtmlReporter({
+            baseDirectory: 'Reports/screenshots',
+            screenshotsSubfolder: 'images'
+         }).getJasmine2Reporter());
+        let globals = require('protractor');
+        let browser: ProtractorBrowser = globals.browser;
+       await browser.waitForAngularEnabled(false);
+       await browser.driver.manage().window().maximize();
+        await browser.driver.manage().window().setPosition(0, 0);        
+        await browser.manage().timeouts().implicitlyWait(5000);
+   },
+    onComplete: () => {
+        browser.close();
+    }
+};
